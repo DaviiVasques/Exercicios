21b53d73c3e12f95c605826332f84ea268330237
DaviiVasques <davivasques07@gmail.com>
2024-06-03 17:42:32 -0300

    commit inicial

---
+++ ExerciciosArranjos/FirstExercise.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ExercicioOne
+{
+    class FirstExercise
+    {
+            /*Console.Write("Digite a quantidade de pessoas a serem regustradas: ");
+            int n = int.Parse(Console.ReadLine() ?? "0");
+            int j = 1;
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write("Digite a " + j++ + "º altura: " );
+                vect[i] = double.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
+
+
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i]; 
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine($"O peso médio é de {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+            */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Principal
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            
+            //Fazer um programa onde guarda um N numeros de itens e faz a média do preço de intens
+            
+            Console.Write("Digite a quantidade de itens que vai ser adicionados: ");
+            int qtdeItens = int.Parse(Console.ReadLine() ?? "00");
+
+            string[] NomeItens = new string[qtdeItens];
+            double[] PrecoItens = new double[qtdeItens];
+
+            for (int i = 0; i < qtdeItens; i++)
+            {
+                Console.Write("Digite o nome dos itens: ");
+                NomeItens[i] = Console.ReadLine() ?? "";
+                Console.Write("Digite o preço do produto: ");
+                PrecoItens[i] = double.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < qtdeItens; i++)
+            {
+                soma += PrecoItens[i];
+            }
+
+            double avg = soma / qtdeItens;
+
+            string ItensList = "";
+            for (int i = 0; i < NomeItens.Length; i++)
+            {
+                ItensList += NomeItens[i];
+                if (i < NomeItens.Length - 1)
+                ItensList += ", ";
+            }
+
+            string MediaFormatada = avg.ToString("F3", CultureInfo.InvariantCulture);
+            Console.WriteLine($"A média dos preços dos produtos {ItensList} é: {MediaFormatada}");
+            
+        }
+    }
+}
